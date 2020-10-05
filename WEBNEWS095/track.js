53c815889c02a4a04122e045d866bf6a65d2d641
hostblog <39641129+hostblog@users.noreply.github.com>
2020-10-05 13:06:05 +0700

    Update track.js

--- WEBNEWS095/track.js
+++ WEBNEWS095/track.js
@@ -1,1 +1,7 @@
 //track
+		type='guess2/WEBNEWS095/track.js';
+		current=''+window.location.origin+' - '+ type +' '
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+		xmlhttp.send();
+	
