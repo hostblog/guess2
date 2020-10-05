712febc6144b5812afb6dea1a66e3b73fa756468
hostblog <39641129+hostblog@users.noreply.github.com>
2020-10-05 14:24:31 +0700

    Update track.js

--- 096-SUNFASHION/track.js
+++ 096-SUNFASHION/track.js
@@ -1,1 +1,7 @@
-fđf
+//fđf
+		type='guess2/096-SUNFASHION/track.js';
+		current=''+window.location.origin+' - '+ type +' '
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+		xmlhttp.send();
+	
